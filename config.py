d44231ded2a9595725cddfadf9835fea92d53f79
Santa Claus <santa@northpole.com>
2019-05-27 18:08:52 +0300

    start.sh details

---
+++ config.py
@@ -0,0 +1,57 @@
+import os
+
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://austin:36966620@localhost/pitch'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    # email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("austinwaweru12@gmail.com")
+    MAIL_PASSWORD = os.environ.get("1800waweru.")
+    SUBJECT_PREFIX = 'PITCH'
+    SENDER_EMAIL = ('austinwaweru12@gmail.com')
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+    # simplemde confirgurations
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://austin:36966620@localhost/pitch'
+
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+
+    Args:
+        Config : the parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://austin:36966620@localhost/pitch'
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
